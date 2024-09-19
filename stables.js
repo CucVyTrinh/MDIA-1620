865033f37ffd08e6450f2eeff650d5a25b6a5b54
CucVyTrinh <trinhcucvy906@gmail.com>
2024-09-19 15:43:08 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,11 @@
+console.log("horse are great!")
+let horseName = "Coolguy"; // this is a string
+horseName = "CucPhuong"; // for strings, prefer double quotes
+horseAge = 5;
+console.log(horseName);
+console.log(horseAge);
+let isHorseInStable = true;
+console.log(isHorseInStable);
+console.log(horseName);
+horseName = "Coolhorse"
+console.log(horseName);
