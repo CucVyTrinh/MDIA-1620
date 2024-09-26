791207138956cc1d820515a88e0874ef10ef5465
CucVyTrinh <trinhcucvy906@gmail.com>
2024-09-26 14:37:25 -0700

    add operators to my stable

--- stables.js
+++ stables.js
@@ -1,11 +1,23 @@
 console.log("horse are great!")
-let horseName = "Coolguy"; // this is a string
+let horseName = "CucPhuong"; // this is a string
 horseName = "CucPhuong"; // for strings, prefer double quotes
+let horseAge="5";
 horseAge = 5;
 console.log(horseName);
 console.log(horseAge);
 let isHorseInStable = true;
 console.log(isHorseInStable);
 console.log(horseName);
-horseName = "Coolhorse"
+horseName = "CucPhuong"
 console.log(horseName);
+
+const STABLE_MONTHLY_FEE = 100;
+let horseIntroduction="is the name of my horse";
+// let rentIntroduction = " it costs " + STABLE_MONTHLY_FEE + " to board " + horseName;
+let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
+
+//let stableIntroduction = "Welcome to " + horseName+ "'s stable";
+let stableIntroduction = `Welcome to "${horseName}'s" stable`;
+console.log(rentIntroduction);
+
+
