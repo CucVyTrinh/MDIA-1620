a8b8095dd2a7d966515e70aed3867eaeb0d7dfd4
CucVyTrinh <trinhcucvy906@gmail.com>
2024-10-31 12:45:09 -0700

    This is lesson 7

--- stables.js
+++ stables.js
@@ -1,18 +1,18 @@
-console.log("horse are great!")
+console.log("horse are great!");
 let horseName = "CucPhuong"; // this is a string
 horseName = "CucPhuong"; // for strings, prefer double quotes
-let horseAge="5";
+let horseAge = "5";
 horseAge = 5;
 console.log(horseName);
 console.log(horseAge);
 let isHorseInStable = true;
 console.log(isHorseInStable);
 console.log(horseName);
-horseName = "CucPhuong"
+horseName = "CucPhuong";
 console.log(horseName);
 
 const STABLE_MONTHLY_FEE = 100;
-let horseIntroduction="is the name of my horse";
+let horseIntroduction = "is the name of my horse";
 // let rentIntroduction = " it costs " + STABLE_MONTHLY_FEE + " to board " + horseName;
 let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
 
@@ -21,39 +21,19 @@ let stableIntroduction = `Welcome to "${horseName}'s" stable`;
 console.log(rentIntroduction);
 
 /////lab 4 code/////
-let horseNickName="CucCut";
+let horseNickName = "CucCut";
 console.log(`His name is ${horseName} but you can call him ${horseNickName}`);
 console.log(`${horseNickName} is very cute`);
 
-let totalCost = `${(STABLE_MONTHLY_FEE*(10/100))*3}`;
+let totalCost = `${STABLE_MONTHLY_FEE * (10 / 100) * 3}`;
 console.log(`I would save ${totalCost}$ after 3 months`);
 
-
-
 ///LESSON 5////
-let isHorseInside=true;
-if (isHorseInside) [
-    console.log(horseName + " is inside")
-]
- let word = "Rainbow";
- console.log(world.ChartArt(0));
-
-
-//////////////////Lesson 5 - Lab
-let firstHorse = "Meo";
-if ((horseName === "CucPhuong" && isHorseInStable) && (firstHorseHorse === "Strawberry" || firstHorse === "Meo")){
-    console.log(`${horseName} outside, ${firstHorse} playing`);
-} else if ((horseName === "CucPhuong" && !isHorseInStable) && (firstHorse === "Strawberry" || firstHorse === "Meo")) {
-    console.log(`${horseName} is playing with ${firstHorse}`);
-} else {
-    console.log(`${horseName} is playing alone`);
-}
-
-
-
+let isHorseInside = true;
+if (isHorseInside) [console.log(horseName + " is inside")];
 
 /////Lesson 6/////
-let firstHorseName = "Vinh"; 
+let firstHorseName = "Vinh";
 let secondHorseName = "Na";
 let thirdHorseName = "Ximai";
 
@@ -61,13 +41,10 @@ console.log(`The first horse is ${firstHorseName}, he is very cute`);
 console.log(`The second horse is ${secondHorseName}, she is very shy`);
 console.log(`The third horse is ${thirdHorseName}, he is very energetic`);
 
-let inside=true;
-if (inside) [
-    console.log(firstHorseName + " is inside")
-]
-if (inside) [
-    console.log(secondHorseName + "is outside")
-]
-if (inside) [
-    console.log(thirdHorseName) + "is inside"
-]
+let inside = true;
+if (inside) [console.log(firstHorseName + " is inside")];
+if (inside) [console.log(secondHorseName + "is outside")];
+if (inside) [console.log(thirdHorseName) + "is inside"];
+
+
+
